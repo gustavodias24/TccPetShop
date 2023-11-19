5fed2787ad2a05436b452d4fc8bab8c316c9cf2f
gusta <gustavo024dias@gmail.com>
2023-11-19 20:43:02 -0300

    OK - Funcionalidade de comprar um produto e fazer as referências de usuário, loja e  produto. OK - Funcionalidade de atualizar estoque quando houver uma compra.

--- app/src/main/java/benicio/soluces/tccpetshop/ui/FragmentCart.java
+++ app/src/main/java/benicio/soluces/tccpetshop/ui/FragmentCart.java
@@ -2,11 +2,20 @@ package benicio.soluces.tccpetshop.ui;
 
 
 import android.annotation.SuppressLint;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +29,7 @@ import benicio.soluces.tccpetshop.utils.CarrinhoUtils;
 
 public class FragmentCart extends BaseFragment<FragmentCartBinding> {
 
+    DatabaseReference refProduct = FirebaseDatabase.getInstance().getReference().child("product_table");
     public FragmentCart() { super(R.layout.fragment_cart, FragmentCartBinding::bind);}
 
     List<ProductModel> produtos = new ArrayList<>();
@@ -36,15 +46,73 @@ public class FragmentCart extends BaseFragment<FragmentCartBinding> {
                 CarrinhoUtils.saveCarrinho(new ArrayList<>(), requireContext());
                 produtos.clear();
                 adapter.notifyDataSetChanged();
+                binding.limparCarrinho.setVisibility(View.GONE);
+            });
+
+            binding.btnComprar.setOnClickListener( view -> {
+                if ( produtos.isEmpty() ){
+                    Toast.makeText(getContext(), "Nenhum produto selecionado!", Toast.LENGTH_SHORT).show();
+                }else{
+                    atualizarEstoqueProdutos();
+                    Toast.makeText(getContext(), "Carregando...", Toast.LENGTH_SHORT).show();
+                }
             });
     }
 
+    private void atualizarEstoqueProdutos(){
+
+        refProduct.addListenerForSingleValueEvent(new ValueEventListener() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if ( snapshot.exists() ){
+                    for ( DataSnapshot dado : snapshot.getChildren()){
+                        ProductModel productModel = dado.getValue(ProductModel.class);
+
+                        for ( ProductModel produtoCart : produtos){
+                            if ( produtoCart.getId() == productModel.getId()){
+                                int quantiVedida = productModel.getquantiVenda();
+                                quantiVedida++;
+                                int quantidadeEstoque = productModel.getQuanti();
+                                if ( quantidadeEstoque > 0){
+                                    quantidadeEstoque--;
+                                }
+
+                                productModel.setquantiVenda(
+                                       quantiVedida
+                                );
+
+                                productModel.setQuanti(
+                                        quantidadeEstoque
+                                );
+
+                                refProduct.child(productModel.getId() + "").setValue(productModel);
+                            }
+                        }
+                    }
+
+                    CarrinhoUtils.saveCarrinho(new ArrayList<>(), requireContext());
+                    produtos.clear();
+                    adapter.notifyDataSetChanged();
+                    getBinding().limparCarrinho.setVisibility(View.GONE);
+                    Toast.makeText(getContext(), "Compra realizada com sucesso!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
     private void configurarRecyclerView(){
         r = getBinding().recylerCarrinho;
         r.setLayoutManager(new LinearLayoutManager(getContext()));
         r.addItemDecoration(new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL));
         r.setHasFixedSize(true);
         produtos.addAll(CarrinhoUtils.returnCarrinho(requireContext()));
+        if ( produtos.isEmpty()) { getBinding().limparCarrinho.setVisibility(View.GONE);}
         adapter = new AdapterProdutos(produtos, getContext(), true);
         r.setAdapter(adapter);
     }
